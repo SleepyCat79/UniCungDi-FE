7a4b53c43111d4df95436f432407c177bc61fb18
hungnviet <nvh100904@gamil.com>
2024-01-29 00:16:36 +0700

    config tailwind css

--- src/screens/landing_screen/landing.tsx
+++ src/screens/landing_screen/landing.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Landing() {
-  return <div className="App">landing</div>;
+  return (
+    <div className="p-5" >
+      <div>
+        <p className="lg: hidden">it for phone</p>
+        <p className="">it for computer</p>
+      </div>
+
+    </div>
+  )
 }
 
 export default Landing;
