979f901598df086cf7d86e7a03ebd3106b6c9565
Sleepy <60354077+SleepyCat79@users.noreply.github.com>
2024-02-03 01:05:49 +0700

    add logo

---
+++ src/screens/authentication_screen/signup2.tsx
@@ -0,0 +1,9 @@
+import exp from "constants";
+import React from "react";
+import { IoMdMail } from "react-icons/io";
+import { Link } from "react-router-dom";
+
+function SignUp2() {
+  return <div></div>;
+}
+export default SignUp2;
